e25172817b6397bef03f61261b0d12de5541bec1
Vinicius <contato.santosvinicius@gmail.com>
2024-09-21 13:22:02 -0300

    Insert the layout of App with Header,Footer and MainContent

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; 2024 Task List App</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1>Task List</h1>
+    </header>
+  );
+};
+
+export default Header;
